c3efddc24b959768f3a0abb65930214c0f111de7
Fabia <fabia@gmail>
2025-07-15 00:31:36 -0300

    Finalizando etapa adicionando as páginas de contato e formulário enviado, junto da requisição necessária e a estilização das páginas

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const __dirname = dirname(__filename);
 
 app.use(express.static("public"));
 app.get("/", (requisicao, resposta) => {
-  resposta.sendFile(path.join(__dirname, "views", "index.html"));
+  resposta.status(200).sendFile(path.join(__dirname, "views", "index.html"));
 });
+
+app.get("/contato", (requisicao, resposta) => {
+  resposta.status(200).sendFile(path.join(__dirname, "views", "contato.html"));
+});
+
+app.get("/formularioEnviado", (requisicao, resposta) => {
+  resposta
+    .status(200)
+    .sendFile(path.join(__dirname, "views", "formularioEnviado.html"));
+});
+
 export default app;
--- public/script/script.js
+++ public/script/script.js
@@ -1,25 +1,29 @@
 const lanchesDiv = document.getElementById("lanches");
 
-function PegarDados() {
-  const resposta = () => {
-    fetch("http://localhost:3333");
-    const conteudo = resposta.json();
-    return conteudo;
-  };
+function ExibirAlerta() {
+  alert("Novo lanche adicionado!");
+}
+
+async function PegarDados() {
+  const resposta = await fetch("/data/lanches.json");
+  const conteudo = await resposta.json();
+  return conteudo;
 }
 
 function InserirElementosHTML(item) {
-  const nome = createElement("h3");
-  const ingredientes = createElement("li");
+  const nome = document.createElement("h3");
+  const ingredientes = document.createElement("li");
   nome.innerHTML = item.nome;
   ingredientes.innerHTML = item.ingredientes;
   lanchesDiv.appendChild(nome);
   lanchesDiv.appendChild(ingredientes);
 }
 
-function ExibirHTML() {
-  const dadosLanches = PegarDados();
-  dadosLanches.map((lanche) => {
+async function ExibirHTML() {
+  const dadosLanches = await PegarDados();
+  dadosLanches.lanches.forEach((lanche) => {
     InserirElementosHTML(lanche);
   });
 }
+
+ExibirHTML();
